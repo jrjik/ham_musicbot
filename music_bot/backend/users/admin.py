6406500fdbc1ad44cd7bfe5b40cee87a6b44b475
jrjik <sasha.kovalyov.2005@inbox.ru>
2025-06-16 15:10:27 +0300

    django admin panel add

--- music_bot/backend/users/admin.py
+++ music_bot/backend/users/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+from users.models import TelegramUser
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('telegram_id',)
+    search_fields = ('telegram_id',)
