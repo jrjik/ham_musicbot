240ad4ac4708c1673f05607489c67ab84a605989
jrjik <sasha.kovalyov.2005@inbox.ru>
2025-06-18 14:29:24 +0300

    add dynamic description to go_to_search

--- music_bot/bot/screens/go_to_search.py
+++ music_bot/bot/screens/go_to_search.py
@@ -3,6 +3,7 @@
 from typing import TYPE_CHECKING
 
 import screens
+from client.backend_client import API_CLIENT
 from hammett.core import Button
 from hammett.core.constants import SourceTypes
 from screens import BaseScreen
@@ -17,6 +18,27 @@ if TYPE_CHECKING:
 
 class GoToSearch(BaseScreen):
     """В классе содержится кнопка возврата на StartScreen."""
+
+    async def get_description(
+        self: 'Self',
+        update: 'Update | None',
+        _context: 'CallbackContext[BT, UD, CD, BD]',
+    ) -> str:
+        """Метод для передачи в сообщение описания текущего списка исполнителя,
+        по которому будет парсинг.
+        """
+        user_id = update.effective_user.id
+        artist_list = await API_CLIENT.get_user_list(user_id)
+
+        if not artist_list:
+            return ('У вас пока нет добавленных исполнителей. '
+                    'Добавьте их, чтобы начать поиск релизов.')
+
+        artist_lines = '\n\n'.join(artist_list)
+        return (
+            '🔎 Нажав на кнопку «К поиску», вы можете найти релизы для следующих исполнителей:\n\n'
+            f'{artist_lines}'
+        )
 
     async def add_default_keyboard(
         self: 'Self',
